70579ac2843e10ae1e9d1f864387ba0298d7e325
jon <jon6566@gmail.com>
2023-05-16 05:10:31 -0700

    .

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const userImput = document.querySelector("#imp");
-const board = document.querySelector('results');
+const board = document.querySelector('#results');
 
 
 
 async function getData(){
-   
+   if(userImput.value){
     const param = {params: 
         {q:userImput.value,
         api_key:"gKJEoaZD6DtRLW5XbIK2VUhI4o96SYu0"}}
     const res = await axios.get('http://api.giphy.com/v1/gifs/search',param)
-        console.log(res.data);
-        console.log(res.data[0])
+        console.log(res);
+        console.log(res.data.data[0])
     
-    const newImage = document.createElement("img");
-    //let ind = Math.floor(Math.random() * res.data.length)
-    //newImage.src =res.data[3].images.original.url ;
+   const newImage = document.createElement("img");
+    let ind = Math.floor(Math.random() * res.data.data.length)
+    newImage.src =res.data.data[ind].images.original.url ;
     board.appendChild(newImage)
-
 }
+}
+// const add =document.querySelector("#add");
+// const del = document.querySelector('#rem');
+
+// //add.addEventListener('click',function(){
+//  //   console.log("you pussed it")
+// //})
+
+// del.addEventListener("click", function(){
+//     board.empty()
+// })
